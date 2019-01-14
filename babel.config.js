76c1647145dc7aa371c3bbc6d5fa11a2ea8d9713
Matthew Shelley <dewayne.shelley@gmail.com>
2019-01-13 20:55:09 -0800

    workign on jest / babel polyfill issue

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,8 @@ module.exports = function(api) {
     ['@babel/plugin-proposal-class-properties', { loose: true }], // eslint-disable-line
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-transform-async-to-generator'
+    '@babel/plugin-transform-async-to-generator',
+    '@babel/plugin-transform-regenerator'
   ];
 
   const presets = ['@babel/preset-env', '@babel/preset-typescript'];
